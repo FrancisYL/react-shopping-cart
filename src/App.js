3db90227349cf10566a5aabeaa9dc3c1ec0dff4f
FrancisYL <yiansu66@gmail.com>
2019-05-06 21:51:57 -0500

    implement static page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,51 @@
 import React from 'react';
-import './App.css';
+import Grid from '@material-ui/core/Grid';
 
-const App = ({products}) => {
+const sizes = ['S', 'M', 'L', 'XL'];
+const orders = ['----- Select -----', 'Lowest to Highest', 'Highest to Lowest'];
+
+const SizeSelector = () => (
+  <div className="size-selection">
+    { Object.values(sizes).map(size => <button className="size-button">{ size }</button>)}
+  </div>
+)
+
+const OrderSelector = () => (
+  <div className="order-selection">
+    { 'Order by ' }
+    <select>
+      { Object.values(orders).map(order => <option value={ order }>{order}</option> )}
+    </select>
+  </div>
+)
+
+const ProductCard = ({ product }) => (
+  <Grid item xs={3}>
+    <img alt={ product.sku } src={require('../public/data/images/'+product.sku+'_1.jpg')} />
+    { product.description} { product.title }  $:{ product.price }
+  </Grid>
+)
+
+const ProductTable = ({ products }) => {
   const skus = Object.keys(products);
-  const items = skus.map(sku => <li>{products[sku].title}</li>);
-  return <ul>{items}</ul>;
+
+  return (
+    <div className="product-table">
+      <Grid container spacing={16} justify="center">
+        { skus.map(sku => <ProductCard key={ sku } product={ products[sku] } />) }
+      </Grid>
+    </div>
+  );
+}
+
+const App = ({ products }) => {
+  return (
+    <div className="shopping-page">
+      <SizeSelector />
+      <OrderSelector />
+      <ProductTable products={ products } />
+    </div>
+  );
 }
 
 export default App;
